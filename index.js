8c4815271614b55cd065da800682d4981c7e75c7
Evie Hoang <evie.h0325@gmail.com>
2023-10-30 22:03:16 -0700

    Added promise callback for banner, initialized prompts.

--- index.js
+++ index.js
@@ -2,32 +2,37 @@
 const mysql = require('mysql2');
 // figlet for ascii images
 const figlet = require('figlet');
-
+// prompts
+const prompts = require('./prompts');
+const inquirer = require('inquirer');
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'company_db'
-  },
-  console.log(`Connected to company_db database.`)
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'company_db'
+    },
+    console.log(`Connected to company_db database.`)
 );
 
-function banner(callback) {
-    figlet.text("FISH-SEA CO.", {
-        font: "Bubble",
-    }, function (err, data) {
+function banner() {
+    return new Promise((resolve, reject) => {
+      figlet.text("FISH-SEA CO.", {
+        font: "Bubble"
+      }, function (err, data) {
         if (err) {
-            console.log("Something went wrong...");
-            console.dir(err);
-            return;
+          console.log("Something went wrong...");
+          console.dir(err);
+          reject(err);
+        } else {
+          console.log(data);
+          resolve();
         }
-        console.log(data);
-        callback();
+      });
     });
-};
+  }
 
 const sharky =
     `
@@ -51,4 +56,17 @@ function shark() {
 
 
 // Launching Functions
-banner(shark);
+
+async function start() {
+    try {
+        await banner();
+        shark();
+        inquirer.prompt(prompts.menu).catch(err => {
+            console.log(err);
+        });
+    } catch (err) {
+        console.error('An error occurred:', err);
+    }
+}
+
+start();
--- prompts.js
+++ prompts.js
@@ -1,3 +1,22 @@
+// prompts to ask for employee information on the menu
+// Presented with the following options: view all departments, view all roles, view all employees, 
+// add a department, add a role, add an employee, and update an employee role
 const inquirer = require('inquirer');
 
-// prompts to ask for employee information
+const menu = [{
+    type: "list",
+    name: "options",
+    message: "What would you like to do? (Use Arrow Keys)",
+    choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add Departments",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "Quit",
+    ]
+}]
+
+module.exports = { menu };
