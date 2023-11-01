6b9081b3ba0b53b53811269488798eaca85fe78a
Evie Hoang <evie.h0325@gmail.com>
2023-10-31 19:46:42 -0700

    Added queries for employees and added addDept option

--- db/query.sql
+++ db/query.sql
@@ -1,2 +1,6 @@
 -- File to make and test queries
 SELECT * FROM company_db.departments;
+SELECT id, title, salary, department_id FROM company_db.roles;
+SELECT first_name, last_name, role_id, manager_id FROM company_db.employees;
+
+-- Query for Add department
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const figlet = require('figlet');
 const prompts = require('./prompts');
 const inquirer = require('inquirer');
 
+
 // Connect to database
 const db = mysql.createConnection(
     {
@@ -68,6 +69,18 @@ const choices = function () {
                 return viewDept();
             case "View All Roles":
                 return viewRoles();
+            case "View All Employees":
+                return viewEmp();
+            case "Add Department":
+                return addDept();
+            case "Add Role":
+                return addRole();
+            case "Add Employee":
+                return addEmp();
+            case "Update Employee Role":
+                return updateEmp();
+            case "Quit":
+                return end();
                 break;
         }
     })
@@ -79,7 +92,7 @@ const choices = function () {
 
 
 
-// Back to menu option
+// Back to menu and exit options
 function backOpt() {
     return inquirer.prompt([
         {
@@ -96,22 +109,20 @@ function backOpt() {
 
 const back = function () {
     backOpt()
-    .then(response => {
-        if (response.options === "Back") {
-            choices();
-        } else if (response.options === "Quit") {
-            db.end;
-        }
-    })
-    .catch(err => {
-        console.error(err);
-    });
+        .then(response => {
+            if (response.options === "Back") {
+                choices();
+            } else if (response.options === "Quit") {
+                db.end;
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
 };
 
 
-
-
-
+// View All Dept. Roles, and Employees
 function viewDept() {
     db.query('SELECT department_name FROM company_db.departments;', function (err, results) {
         if (err) {
@@ -131,6 +142,28 @@ function viewRoles() {
             back();
         }
     });
+}
+function viewEmp() {
+    db.query('SELECT first_name, last_name, role_id, manager_id FROM company_db.employees;', function (err, results) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.table(results);
+            back();
+        }
+    });
+}
+
+// Add Dept, Role, or Employee
+async function addDept() {
+    const dept = await inquirer.prompt({
+        type: "input",
+        name: "deptName",
+        message: "What is the name of the new department?",
+    });
+    const result = db.query("INSERT INTO company_db.departments SET ?", dept);
+    console.table(result);
+    back();
 }
 
 // Launching Everything
